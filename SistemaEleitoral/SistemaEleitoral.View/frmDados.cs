a50a4b722aea77ce2aa3132eb25dcef363e2c3fd
Fabricio Rodrigues <fabricio-dsr@hotmail.com>
2015-04-12 08:17:13 -0300

    Grid de dados: Eleitor, Candidato e Mesario funcionando (Necessita BD);

--- SistemaEleitoral/SistemaEleitoral.View/frmDados.cs
+++ SistemaEleitoral/SistemaEleitoral.View/frmDados.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using SistemaEleitoral.Controller;
+using SistemaEleitoral.Model;
+using System.Data.SqlClient;
 
 namespace SistemaEleitoral
 {
@@ -21,5 +24,79 @@ namespace SistemaEleitoral
         {
 
         }
-    }
+
+		private void frmDados_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		private void button12_Click(object sender, EventArgs e)
+		{
+			SqlConnection oCn = Model.Data.Conexao.ConexaoSqlServer();
+			string SQL = "SELECT * FROM Candidato WHERE Numero = @num OR Nome = @nome";
+
+			SqlCommand oComando = new SqlCommand(SQL, oCn);
+			oComando.Parameters.Add("@num", SqlDbType.VarChar).Value = txtNumeroCandidato.Text;
+			oComando.Parameters.Add("@nome", SqlDbType.VarChar).Value = txtNomeCandidato.Text;
+
+			oComando.Connection = oCn;
+			oComando.CommandText = SQL;
+
+			SqlDataAdapter adapter = new SqlDataAdapter();
+			adapter.SelectCommand = oComando;
+
+			DataSet dataSet = new DataSet();
+			adapter.Fill(dataSet);
+
+			dtgCandidatos.DataSource = dataSet;
+			dtgCandidatos.DataMember = dataSet.Tables[0].TableName;
+			oCn.Close();
+		}
+
+		private void btnPesquisaEleitor_Click(object sender, EventArgs e)
+		{
+			SqlConnection oCn = Model.Data.Conexao.ConexaoSqlServer();
+			string SQL = "SELECT * FROM Eleitor WHERE Matricula = @mat OR Nome = @nome";
+
+			SqlCommand oComando = new SqlCommand(SQL, oCn);
+			oComando.Parameters.Add("@mat", SqlDbType.VarChar).Value = txtMatEleitor.Text;
+			oComando.Parameters.Add("@nome", SqlDbType.VarChar).Value = txtNomeEleitor.Text;
+
+			oComando.Connection = oCn;
+			oComando.CommandText = SQL;
+
+			SqlDataAdapter adapter = new SqlDataAdapter();
+			adapter.SelectCommand = oComando;
+
+			DataSet dataSet = new DataSet();
+			adapter.Fill(dataSet);
+
+			dtgEleitores.DataSource = dataSet;
+			dtgEleitores.DataMember = dataSet.Tables[0].TableName;
+			oCn.Close();
+		}
+
+		private void btnPesquisaMesario_Click(object sender, EventArgs e)
+		{
+			SqlConnection oCn = Model.Data.Conexao.ConexaoSqlServer();
+			string SQL = "SELECT * FROM Mesario WHERE Codigo = @cod OR Nome = @nome";
+
+			SqlCommand oComando = new SqlCommand(SQL, oCn);
+			oComando.Parameters.Add("@cod", SqlDbType.VarChar).Value = txtCodigoMesario.Text;
+			oComando.Parameters.Add("@nome", SqlDbType.VarChar).Value = txtNomeMesario.Text;
+
+			oComando.Connection = oCn;
+			oComando.CommandText = SQL;
+
+			SqlDataAdapter adapter = new SqlDataAdapter();
+			adapter.SelectCommand = oComando;
+
+			DataSet dataSet = new DataSet();
+			adapter.Fill(dataSet);
+
+			dtgMesarios.DataSource = dataSet;
+			dtgMesarios.DataMember = dataSet.Tables[0].TableName;
+			oCn.Close();
+		}
+	}
 }
